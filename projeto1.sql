7fc6b7f195099ac3797dbf4cc0bf513a3f028265
Wil220 <wilker.silva0714@gmail.com>
2024-06-26 17:10:45 -0300

    mysql

---
+++ projeto1.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE db_quitanda;
+
+USE db_quitanda;
+
+CREATE TABLE tb_produtos(
+    id BIGINT AUTO_INCREMENT,
+	nome VARCHAR(255) NOT NULL,
+	quantidade INT,
+    datavalidade DATE,
+	preco DECIMAL NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO tb_produtos(nome, quantidade, datavalidade, preco) 
+VALUES ("tomate",100, "2023-12-15", 8.00);
+INSERT INTO tb_produtos(nome, quantidade, datavalidade, preco) 
+VALUES ("maçã",20, "2023-12-15", 5.00);
+INSERT INTO tb_produtos(nome, quantidade, datavalidade, preco) 
+VALUES ("laranja",50, "2023-12-15", 10.00);
+INSERT INTO tb_produtos(nome, quantidade, datavalidade, preco) 
+VALUES ("banana",200, "2023-12-15", 12.00);
+INSERT INTO tb_produtos(nome, quantidade, datavalidade, preco) 
+VALUES ("uva",1200, "2023-12-15", 30.00);
+INSERT INTO tb_produtos(nome, quantidade, datavalidade, preco) 
+VALUES ("pêra",500, "2023-12-15", 2.99);
+
+SELECT * FROM tb_produtos;
+SELECT nome FROM tb_produtos;
+SET SQL_SAFE_UPDATES = 0;
+ALTER TABLE tb_produtos MODIFY preco DECIMAL(6,2);
+UPDATE tb_produtos SET preco = 2.99 WHERE Id = 6;
